8436381a403967993d2323539fcc1921b75142a5
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-22 16:01:27 +0300

    separate the mock data in different model files

--- server.js
+++ server.js
@@ -13,34 +13,8 @@ const schema = makeExecutableSchema({
 });
 
 const root = {
-  products: [
-    {
-      id: "redshoe",
-      description: "Red Shoe",
-      price: 42.12,
-    },
-    {
-      id: "bluejean",
-      description: "Blue Jeans",
-      price: 55.55,
-    },
-  ],
-  orders: [
-    {
-      date: "2005-05-05",
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: "redshoe",
-            description: "Old Red Shoe",
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
+  products: require("./products/products.model"),
+  orders: require("./orders/orders.model"),
 };
 
 const app = express();
